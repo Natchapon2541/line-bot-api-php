d46adf1eec19187946ab40fe8571cc22a8d5110a
Natchapon <50616868+Natchapon2541@users.noreply.github.com>
2020-03-11 19:51:32 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'BvDsy8t/wkYCho4aIR/uK1JJquwnnTI7vnenWiu3Df6TEvLoBptYR7HonAtmHUKDQOffAkWetc4iw4Mkw7u4rlMM/VsBB6A5D7DfZ3NjO9kwWKC6wiHm3vvk86eBVCKUyT1nwxoYwLl0ok5n1JxrhQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = '3EamQzWrjgbp8nNqWx62XOd25uNlE6xun3lVYrGJrWJtaW9BJeAtlfoTdelAz/3UHV4g9of47dtjlsuogCKsvK1DVJsPWmRm2Zvhc+20rMebXaKVDRoanhn7zE5qrT8Huys6uv+leeTne9eDmgyK/gdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
